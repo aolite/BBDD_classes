0e2e7f5d74d87a5ce02950c1d9ab7c0331d537e9
Aitor Corchero Rodríguez <aitorcorchero@gmail.com>
2022-09-26 08:25:26 +0200

    Laboratory 2 database inserts

--- laboratories/laboratory 2/inserts/assignats.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO public.assignats (num_avio, num_vol, data) VALUES (4, 6, '2022-09-22');
-INSERT INTO public.assignats (num_avio, num_vol, data) VALUES (4, 6, '2022-10-20');
-INSERT INTO public.assignats (num_avio, num_vol, data) VALUES (5, 7, '2022-09-22');
-INSERT INTO public.assignats (num_avio, num_vol, data) VALUES (5, 9, '2022-10-19');
-INSERT INTO public.assignats (num_avio, num_vol, data) VALUES (6, 4, '2022-09-28');
-INSERT INTO public.assignats (num_avio, num_vol, data) VALUES (6, 5, '2022-10-12');
--- laboratories/laboratory 2/inserts/bitllets.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO public.bitllets (dni, num_vol, data) VALUES ('78530067V', 1, '2022-09-01');
-INSERT INTO public.bitllets (dni, num_vol, data) VALUES ('78530067V', 1, '2022-10-01');
-INSERT INTO public.bitllets (dni, num_vol, data) VALUES ('A57736324', 6, '2022-09-22');
-INSERT INTO public.bitllets (dni, num_vol, data) VALUES ('B50978246', 7, '2022-09-22');
-INSERT INTO public.bitllets (dni, num_vol, data) VALUES ('A57736324', 6, '2022-10-20');
-INSERT INTO public.bitllets (dni, num_vol, data) VALUES ('B50978246', 9, '2022-10-11');
-INSERT INTO public.bitllets (dni, num_vol, data) VALUES ('Y2354337W', 5, '2022-10-25');
